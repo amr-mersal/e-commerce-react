33f4319968069c208067675f660f09e72760b09d
amr mersal <amral3117@gmail.com>
2025-02-12 12:38:34 +0200

    h

--- src/Pages/Category/AllCategoryPage.js
+++ src/Pages/Category/AllCategoryPage.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import CategoryContainer from '../../Components/Category/CategoryContainer'
 import Pagination from '../../Components/Uitlty/Pagination'
+// import baseUrl from '../../Api/baseURL'
 
 const AllCategoryPage = () => {
+    // const get=async()=>{
+    //     const res=await baseUrl.get("/api/v1/categories")
+    //     console.log(res.data)
+    // }
+    // useEffect(()=>{
+    //     get()
+    // },[])
     return (
         <div style={{minHeight:'670px'}}>
         
